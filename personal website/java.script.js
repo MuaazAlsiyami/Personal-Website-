3b89a2cde81e30cdebdd33519ee03a8b4b242414
MuaazAlsiyami <150880475+MuaazAlsiyami@users.noreply.github.com>
2024-11-05 18:15:08 +0700

    Update java.script.js

--- personal website/java.script.js
+++ personal website/java.script.js
@@ -4,15 +4,13 @@ document.querySelector('#search-btn').onclick = () =>{
   searchForm.classList.toggle('active');
 }
 
-let loginForm = document.querySelector('.login-form-container');
+let loginForm = document.querySelector('.login-form-contain
 
 document.querySelector('#login-btn').onclick = () =>{
   loginForm.classList.toggle('active');
 }
 
-document.querySelector('#close-login-btn').onclick = () =>{
-  loginForm.classList.remove('active');
-}
+document.querySelector('#close-login-btn').onclick = () =>{}
 
 window.onscroll = () =>{
 
